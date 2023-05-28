a2dd8b6464b81e87728c6f04f6cc0c2e6f7f8e92
gngpp <gngppz@gmail.com>
2023-05-28 13:59:48 +0000

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,63 @@
-use clap::{command, Parser};
+use clap::{command, Parser, Subcommand};
 use std::{io::Write, sync::Once};
 
-#[derive(Parser)]
+pub mod openai;
+
+#[derive(Parser, Debug)]
 #[clap(author, version, about, arg_required_else_help = true)]
-#[command(args_conflicts_with_subcommands = true)]
 struct Opt {
     /// Enable debug
     #[clap(long, global = true, value_parser = initialize_log)]
     debug: bool,
+
+    /// Proxy. Format: protocol://user:pass@ip:port
+    #[clap(short, long)]
+    proxy: Option<String>,
+
+    /// OpenAI gpt-3.5-turbo chat api, Note: OpenAI will bill you
+    #[clap(short = 'T', long, env = "OPENGPT_TOKEN")]
+    turbo: Option<String>,
+
+    /// OpenAI account email, Format: gngppz@gmail.com
+    #[arg(
+        short = 'E',
+        long,
+        env = "OPENGPT_EMAIL",
+        requires = "password",
+    )]
+    email: Option<String>,
+
+    /// OpenAI account password
+    #[arg(short = 'W', long, env = "OPENGPT_PASSWORD", requires = "email")]
+    password: Option<String>,
+
+    #[clap(subcommand)]
+    command: Option<SubCommands>,
+}
+
+#[derive(Subcommand, Debug)]
+enum SubCommands {
+    /// Start proxy server
+    Server {
+        /// Server Listen host
+        #[clap(short = 'H', long, default_value = "0.0.0.0", value_parser = parser_host)]
+        host: std::net::IpAddr,
+        /// Server Listen port
+        #[clap(short = 'P', long, default_value = "7999", value_parser = parser_port_in_range)]
+        port: u16,
+    },
+    /// Start configuration
+    Config {
+        /// Specify an access token storage file path
+        #[clap(short, long)]
+        token_storage: Option<String>,
+    },
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    Opt::parse();
-
+    let opt = Opt::parse();
+    println!("{:#?}", opt);
     println!("Hello, world!");
     Ok(())
 }
@@ -40,4 +84,29 @@ fn initialize_log(s: &str) -> anyhow::Result<bool> {
             .init();
     });
     Ok(debug)
+}
+
+const PORT_RANGE: std::ops::RangeInclusive<usize> = 1024..=65535;
+
+// port range parser
+fn parser_port_in_range(s: &str) -> anyhow::Result<u16> {
+    let port: usize = s
+        .parse()
+        .map_err(|_| anyhow::anyhow!(format!("`{}` isn't a port number", s)))?;
+    if PORT_RANGE.contains(&port) {
+        return Ok(port as u16);
+    }
+    anyhow::bail!(format!(
+        "Port not in range {}-{}",
+        PORT_RANGE.start(),
+        PORT_RANGE.end()
+    ))
+}
+
+// address parser
+fn parser_host(s: &str) -> anyhow::Result<std::net::IpAddr> {
+    let addr = s
+        .parse::<std::net::IpAddr>()
+        .map_err(|_| anyhow::anyhow!(format!("`{}` isn't a ip address", s)))?;
+    Ok(addr)
 }
