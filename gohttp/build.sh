fb6ab76e8cb0c924b9a4fbfedb13d8cb943c9f76
gngpp <gngppz@gmail.com>
2023-06-02 14:13:58 +0000

    Update

--- gohttp/build.sh
+++ gohttp/build.sh
@@ -5,5 +5,5 @@ set -e
 echo "Start building the  platform static library"
 
 cd ffi
-go build -ldflags "-s -w" -buildmode=c-archive -o libgohttp.a
+GOOS=$GOOS GOARCH=$GOARCH CGO_ENABLED=1 go build -ldflags "-s -w" -buildmode=c-archive -o libgohttp.a
 cd -
--- gohttp/ffi/go.mod
+++ gohttp/ffi/go.mod
@@ -1,4 +1,4 @@
-module github.com/linweiyuan/go-chatgpt-api
+module gohttp
 
 go 1.18
 
