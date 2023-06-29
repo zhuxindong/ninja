34b502a83696f80dcf62e2b7792a603f235525d8
gngpp <gngppz@gmail.com>
2023-06-29 19:25:50 +0800

    Update example

--- examples/oauth.rs
+++ examples/oauth.rs
@@ -6,7 +6,7 @@ use openai::auth::OAuthAccountBuilder;
 async fn main() -> anyhow::Result<()> {
     let email = std::env::var("EMAIL")?;
     let password = std::env::var("PASSWORD")?;
-    let mut auth = openai::auth::OAuthClientBuilder::builder()
+    let auth = openai::auth::OAuthClientBuilder::builder()
         .user_agent(openai::HEADER_UA)
         .chrome_builder(reqwest::browser::ChromeVersion::V108)
         .cookie_store(true)
