1ace0f5c78e741145752097cac011bb65206aa33
gngpp <gngppz@gmail.com>
2023-07-07 08:33:13 +0800

    Add docker builder image

---
+++ docker/builder/builder.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+declare -A map
+map["x86_64-unknown-linux-musl"]="x86_64-musl"
+map["aarch64-unknown-linux-musl"]="aarch64-musl"
+map["armv7-unknown-linux-musleabi"]="armv7-musleabi"
+map["armv7-unknown-linux-musleabihf"]="armv7-musleabihf"
+map["arm-unknown-linux-musleabi"]="arm-musleabi"
+map["arm-unknown-linux-musleabihf"]="arm-musleabihf"
+
+for key in "${!map[@]}"; do
+  docker buildx build --platform linux/amd64,linux/arm64 \
+    --tag gngpp/opengpt-builder:"$key" \
+    --tag ghcr.io/gngpp/opengpt-builder:"$key" \
+    --build-arg BASE_IMAGE=messense/rust-musl-cross:"${map[$key]}" . --push 
+done
