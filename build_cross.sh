57b838c75f596d2749481256b3b39d8af2f0f3e1
gngpp <gngppz@gmail.com>
2023-07-08 12:32:33 +0800

    Update build_cross.sh

--- build_cross.sh
+++ build_cross.sh
@@ -6,6 +6,7 @@ root=$(pwd)
 : ${tag=latest}
 [ ! -d uploads ] && mkdir uploads
 
+cargo update
 cargo install cargo-deb
 
 target_list=(x86_64-unknown-linux-musl aarch64-unknown-linux-musl armv7-unknown-linux-musleabi armv7-unknown-linux-musleabihf arm-unknown-linux-musleabi arm-unknown-linux-musleabihf)
@@ -13,8 +14,12 @@ for target in ${target_list[@]}; do
     docker pull ghcr.io/gngpp/opengpt-builder:$target
     docker run --rm -t \
         -v $(pwd):/home/rust/src \
+        -v $HOME/.cargo/registry:/root/.cargo/registry \
+        -v $HOME/.cargo/git:/root/.cargo/git \
+        --prevailed
         ghcr.io/gngpp/opengpt-builder:$target cargo build --release        
-    upx --lzma target/$target/release/opengpt
+    sudo chmod 755 target/$target/release/opengpt
+    sudo upx --lzma target/$target/release/opengpt
 
     cargo deb --target=$target --no-build --no-strip
     cd target/$target/debian
