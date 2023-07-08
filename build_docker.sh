02c2aa68b976a7c4bf482987850ee56243c8f13f
gngpp <gngppz@gmail.com>
2023-07-08 14:47:35 +0800

    update docker build

--- build_docker.sh
+++ build_docker.sh
@@ -3,7 +3,7 @@
 : ${tag=latest}
 
 cd docker
-docker buildx build --platform linux/amd64,linux/arm64,linux/arm32v7,linux/arm32v5 \
+docker buildx build --platform linux/amd64,linux/arm64/v8,linux/arm/v7,linux/arm/v6 \
     --tag ghcr.io/gngpp/opengpt:$tag \
     --tag gngpp/opengpt:$tag \
     --tag gngpp/opengpt:latest \
