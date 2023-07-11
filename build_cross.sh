2c6e4920f2e068e7c02a47e83e131c3eb120cc7a
gngpp <gngppz@gmail.com>
2023-07-11 19:54:39 +0800

    Update x86_64-pc-windows-msvc target build

--- build_cross.sh
+++ build_cross.sh
@@ -12,11 +12,21 @@ cargo install cargo-deb
 target_list=(x86_64-unknown-linux-musl aarch64-unknown-linux-musl armv7-unknown-linux-musleabi armv7-unknown-linux-musleabihf armv5te-unknown-linux-musleabi arm-unknown-linux-musleabi arm-unknown-linux-musleabihf x86_64-pc-windows-msvc)
 for target in ${target_list[@]}; do
     docker pull ghcr.io/gngpp/opengpt-builder:$target
-    docker run --rm -t --privileged \
-        -v $(pwd):/home/rust/src \
-        -v $HOME/.cargo/registry:/root/.cargo/registry \
-        -v $HOME/.cargo/git:/root/.cargo/git \
-        ghcr.io/gngpp/opengpt-builder:$target cargo build --release        
+
+    if [ "$target" = "x86_64-pc-windows-msvc" ]; then
+        docker run --rm -t \
+            -v $(pwd):/home/rust/src \
+            -v $HOME/.cargo/registry:/usr/local/cargo/registry \
+            -v $HOME/.cargo/git:/usr/local/cargo/git \
+            ghcr.io/gngpp/opengpt-builder:$target cargo xwin build --release --target x86_64-pc-windows-msvc
+    else
+        docker run --rm -t \
+            -v $(pwd):/home/rust/src \
+            -v $HOME/.cargo/registry:/root/.cargo/registry \
+            -v $HOME/.cargo/git:/root/.cargo/git \
+            ghcr.io/gngpp/opengpt-builder:$target cargo build --release
+    fi
+
     sudo chmod -R 777 target
     sudo upx --lzma target/$target/release/opengpt
 
--- docker/builder/builder.sh
+++ docker/builder/builder.sh
@@ -2,14 +2,14 @@
 
 declare -A map
 
-map["x86_64-unknown-linux-musl"]="messense/rust-musl-cross:x86_64-musl"
-map["aarch64-unknown-linux-musl"]="messense/rust-musl-cross:aarch64-musl"
-map["armv7-unknown-linux-musleabi"]="messense/rust-musl-cross:armv7-musleabi"
-map["armv7-unknown-linux-musleabihf"]="messense/rust-musl-cross:armv7-musleabihf"
-map["arm-unknown-linux-musleabi"]="messense/rust-musl-cross:arm-musleabi"
-map["arm-unknown-linux-musleabihf"]="messense/rust-musl-cross:arm-musleabihf"
-map["armv5te-unknown-linux-musleabi"]="messense/rust-musl-cross:armv5te-musleabi"
-map["x86_64-pc-windows-msvc"]="messense/cargo-xwin:latest"
+map["x86_64-unknown-linux-musl"]="ghcr.io/messense/rust-musl-cross:x86_64-musl"
+map["aarch64-unknown-linux-musl"]="ghcr.io/messense/rust-musl-cross:aarch64-musl"
+map["armv7-unknown-linux-musleabi"]="ghcr.io/messense/rust-musl-cross:armv7-musleabi"
+map["armv7-unknown-linux-musleabihf"]="ghcr.io/messense/rust-musl-cross:armv7-musleabihf"
+map["arm-unknown-linux-musleabi"]="ghcr.io/messense/rust-musl-cross:arm-musleabi"
+map["arm-unknown-linux-musleabihf"]="ghcr.io/messense/rust-musl-cross:arm-musleabihf"
+map["armv5te-unknown-linux-musleabi"]="ghcr.io/messense/rust-musl-cross:armv5te-musleabi"
+map["x86_64-pc-windows-msvc"]="ghcr.io/messense/cargo-xwin:latest"
 
 for key in "${!map[@]}"; do
   docker buildx build --platform linux/amd64,linux/arm64 \
