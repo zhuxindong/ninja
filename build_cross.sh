a57bb95cee773fe046f3740c926060f2ad81bb91
gngpp <gngppz@gmail.com>
2023-07-15 23:17:12 +0800

    Update target build

--- build_cross.sh
+++ build_cross.sh
@@ -5,6 +5,7 @@ set -e
 root=$(pwd)
 : ${tag=latest}
 : ${rmi=false}
+: ${os=linux} # linux(and window) or macos
 [ ! -d uploads ] && mkdir uploads
 
 cargo update
@@ -18,7 +19,19 @@ rmi_docker_image() {
     docker rmi ghcr.io/gngpp/opengpt-builder:$1
 }
 
-build_target() {
+build_macos_target() {
+    cargo build --release --target $1
+    sudo chmod -R 777 target
+    cd target/$1/release
+    sudo upx --lzma opengpt
+    tar czvf opengpt-$tag-$1.tar.gz opengpt
+    shasum -a 256 opengpt-$tag-$1.tar.gz >opengpt-$tag-$1.tar.gz.sha256
+    mv opengpt-$tag-$1.tar.gz $root/uploads/
+    mv opengpt-$tag-$1.tar.gz.sha256 $root/uploads/
+    cd -
+}
+
+build_linux_target() {
     docker run --rm -t \
         -v $(pwd):/home/rust/src \
         -v $HOME/.cargo/registry:/root/.cargo/registry \
@@ -55,21 +68,31 @@ build_windows_target() {
     cd -
 }
 
-target_list=(x86_64-unknown-linux-musl aarch64-unknown-linux-musl armv7-unknown-linux-musleabi armv7-unknown-linux-musleabihf armv5te-unknown-linux-musleabi arm-unknown-linux-musleabi arm-unknown-linux-musleabihf x86_64-pc-windows-msvc)
+if [ "$os" = "linux" ]; then
+    target_list=(x86_64-unknown-linux-musl aarch64-unknown-linux-musl armv7-unknown-linux-musleabi armv7-unknown-linux-musleabihf armv5te-unknown-linux-musleabi arm-unknown-linux-musleabi arm-unknown-linux-musleabihf x86_64-pc-windows-msvc)
 
-for target in "${target_list[@]}"; do
-    pull_docker_image "$target"
+    for target in "${target_list[@]}"; do
+        pull_docker_image "$target"
 
-    if [ "$target" = "x86_64-pc-windows-msvc" ]; then
-        build_windows_target "$target"
-    else
-        build_target "$target"
-    fi
+        if [ "$target" = "x86_64-pc-windows-msvc" ]; then
+            build_windows_target "$target"
+        else
+            build_linux_target "$target"
+        fi
 
-    if [ "$rmi" = "true" ]; then
-        rmi_docker_image "$target"
-    fi
-done
+        if [ "$rmi" = "true" ]; then
+            rmi_docker_image "$target"
+        fi
+    done
+fi
+
+if [ "$os" = "macos" ]; then
+    brew install upx
+    target_list=(x86_64-apple-darwin aarch64-apple-darwin)
+    for target in "${target_list[@]}"; do
+        build_macos_target "$target"
+    done
+fi
 
 generate_directory_tree() {
     find "$1" -print | sed -e 's;[^/]*/;|____;g;s;____|; |;g'
