7828c326f6f851b43eadce12c87949d6b677851c
gngpp <gngppz@gmail.com>
2023-09-10 08:51:28 +0800

    Update har example

--- examples/har.rs
+++ examples/har.rs
@@ -2,8 +2,9 @@ use openai::arkose::ArkoseToken;
 
 #[tokio::main]
 async fn main() {
-    let token = ArkoseToken::new_form_har("chat.openai.com.har")
-        .await
-        .unwrap();
+    let token =
+        ArkoseToken::new_form_har("/Users/gngpp/Desktop/incogniton/117.chat.openai.com.har")
+            .await
+            .unwrap();
     println!("{}", token.value())
 }
