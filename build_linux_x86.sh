7d32e6c245d1ab1be99d0dff66816e776bf6d92d
xiaozhu <501801307@qq.com>
2023-10-09 21:56:50 +0800

    更新 build_linux_x86.sh

--- build_linux_x86.sh
+++ build_linux_x86.sh
@@ -12,7 +12,7 @@ source "$HOME/.cargo/env"
 sudo apt-get install gcc g++ cmake libclang-dev -y 
 cargo build --release
 
-
+sudo chmod -R 777 target
 cd target/release
 tar czvf ninja-$tag-linux_x86_64.tar.gz ninja
 shasum -a 256 ninja-$tag-linux_x86_64.tar.gz >ninja-$tag-linux_x86_64.tar.gz.sha256
