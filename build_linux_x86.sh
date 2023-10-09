9a27fd8dbdafac22f360a83233b74b21e02d4427
zhuxindong <501801307@qq.com>
2023-10-09 20:25:41 +0800

    build_linux_x86.sh

--- build_linux_x86.sh
+++ build_linux_x86.sh
@@ -7,7 +7,7 @@ root=$(pwd)
 [ ! -d uploads ] && mkdir uploads
 
 
-echo 1 | curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
+curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh
 sudu apt-get install gcc g++ cmake libclang-dev -y 
 cargo build --release
 
