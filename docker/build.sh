f5606a19df12899aa7d5172c8140ca6166f59d3e
zhuxindong <501801307@qq.com>
2023-10-09 21:35:14 +0800

    Dockerfile

--- docker/build.sh
+++ docker/build.sh
@@ -7,8 +7,3 @@ docker buildx build --platform linux/amd64 \
     --tag zhuxindong/ninja:$tag \
     --tag zhuxindong/ninja:latest \
     --build-arg VERSION=$tag --push .
-
-cd render
-docker buildx build -t gngpp/ninja:warp -t ghcr.io/gngpp/ninja:warp . --push
-cd -
-cd -
