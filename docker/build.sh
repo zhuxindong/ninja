2d610d7bb24b403a263e0ebe0e37a1bd57768938
gngpp <gngppz@gmail.com>
2023-10-10 08:38:49 +0800

    Update workflows

--- docker/build.sh
+++ docker/build.sh
@@ -9,8 +9,4 @@ docker buildx build --platform linux/amd64,linux/arm64/v8,linux/arm/v7,linux/arm
     --tag gngpp/ninja:$tag \
     --tag gngpp/ninja:latest \
     --build-arg VERSION=$tag --push .
-
-cd render
-docker buildx build -t gngpp/ninja:warp -t ghcr.io/gngpp/ninja:warp . --push
-cd -
 cd -
