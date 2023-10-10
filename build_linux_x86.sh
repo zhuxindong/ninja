bfc4f4b412bd1fd2052c904959309f09c447ef0a
zhuxindong <501801307@qq.com>
2023-10-10 09:47:18 +0800

    Update workflows

--- build_linux_x86.sh
+++ build_linux_x86.sh
@@ -5,11 +5,13 @@ set -e
 root=$(pwd)
 : ${tag=latest}
 [ ! -d uploads ] && mkdir uploads
+cargo update
 
 
 curl --proto '=https' --tlsv1.2 https://sh.rustup.rs -sSf | sh -s -- -y
 source "$HOME/.cargo/env"
-sudo apt-get install gcc g++ cmake libclang-dev -y 
+sudo apt-get install gcc g++ cmake libclang-dev libc6 -y 
+
 cargo build --release
 
 sudo chmod -R 777 target
