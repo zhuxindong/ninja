ca94dc711125f2c2f95c109d8d973dacc94559d1
zhuxindong <501801307@qq.com>
2023-10-26 14:15:13 +0800

    softid

--- openai/src/arkose/funcaptcha/solver.rs
+++ openai/src/arkose/funcaptcha/solver.rs
@@ -76,7 +76,7 @@ pub async fn submit_task(submit_task: SubmitSolver<'_>) -> anyhow::Result<Vec<i3
 
     match submit_task.solved {
         Solver::Yescaptcha => {
-            body.soft_id = Some("26299");
+            body.soft_id = Some("18876");
             url.push_str("https://global.yescaptcha.com/createTask")
         }
         Solver::Capsolver => {
