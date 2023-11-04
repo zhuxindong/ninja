605814465e054d5614d0a2dedb207871359f08bc
gngpp <gngppz@gmail.com>
2023-11-04 14:26:29 +0800

    Update upgrade.rs

--- examples/upgrade.rs
+++ examples/upgrade.rs
@@ -1,11 +1,11 @@
 use std::path::PathBuf;
 
 fn main() {
-    let data = std::fs::read("/Users/gngpp/VSCode/ninja/tLfeYLzFNDDz-YFDI_84l.json").unwrap();
+    let data = std::fs::read("/Users/gngpp/Desktop/sRVMr7pl79_Jt3fw8-hEr.json").unwrap();
     let target: Vec<String> = serde_json::from_slice(&data).unwrap();
     let host = "https://cdn.oaistatic.com";
     let client = reqwest::blocking::ClientBuilder::new()
-        .impersonate(reqwest::impersonate::Impersonate::OkHttpAndroid13)
+        .impersonate(reqwest::impersonate::Impersonate::Chrome104)
         .timeout(std::time::Duration::from_secs(60))
         .connect_timeout(std::time::Duration::from_secs(30))
         .cookie_store(true)
@@ -13,9 +13,7 @@ fn main() {
         .unwrap();
 
     for path in target {
-        let req_url = format!("{}/{}", host, path);
-        let resp = client.get(&req_url).send().unwrap();
-        let file_path = PathBuf::from("upgrade").join(path);
+        let file_path = PathBuf::from("upgrade").join(&path);
         if file_path.exists() {
             continue;
         }
@@ -24,12 +22,18 @@ fn main() {
                 std::fs::create_dir_all(p).unwrap();
             }
         }
+        
+        let req_url = format!("{}/{}", host, path);
+        let resp = client.get(&req_url).send().unwrap();
+
         if resp.status().is_success() {
             std::fs::write(file_path, resp.bytes().unwrap()).unwrap();
             println!("downloaded: {}", req_url);
         } else {
             panic!("request failed: {}", req_url);
         }
+
         std::thread::sleep(std::time::Duration::from_secs(3));
+
     }
 }
