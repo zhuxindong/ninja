6c05e66e487f6b06eaad7f0db88d049b0a7bcd87
gngpp <gngppz@gmail.com>
2023-12-01 23:30:28 +0800

    Update patches.sh

--- patches.sh
+++ patches.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-if [ ! -d "patches" ] && [ -n "$GIT_TOKEN" ]; then
+if [ -d "patches" ]; then
+    rm -rf patches
+fi
+
+if [ -n "$GIT_TOKEN" ]; then
     git clone https://gngpp:$GIT_TOKEN@github.com/gngpp/ninja-patches patches
     
     for patch in patches/*.patch; do
