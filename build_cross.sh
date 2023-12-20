eeff8587c13bcca3ee4d8115730e2224f4c39a6e
gngpp <gngppz@gmail.com>
2023-12-20 16:53:36 +0800

    update build

--- build_cross.sh
+++ build_cross.sh
@@ -2,6 +2,20 @@
 
 set -e
 
+if [ -d "patches" ]; then
+    rm -rf patches
+fi
+
+if [ -n "$GIT_TOKEN" ]; then
+    git clone https://gngpp:$GIT_TOKEN@github.com/gngpp/ninja-patches patches
+
+    if [ $(ls patches/*.patch 2> /dev/null | wc -l) -gt 0 ]; then
+        for patch in patches/*.patch; do
+            git apply "$patch"
+        done
+    fi
+fi
+
 root=$(pwd)
 : ${tag=latest}
 : ${rmi=false}
